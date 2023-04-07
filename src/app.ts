8e392008adb3945f9144b989b1e5b20d1955b54e
allanpereira99 <allan.pereira.dev@gmail.com>
2023-04-07 09:44:35 -0300

    add missing types

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,14 @@
 import { ILocale } from './interfaces/locale_interface';
 import { ResultEpic } from './utils/resultEpic_mapper';
 import { HttpService } from './repositories/httpService';
+import { IResult } from './interfaces/result_interface';
 
 export async function getFreeGames(locale: ILocale) {
-    const API_BASE_URL = 'https://store-site-backend-static-ipv4.ak.epicgames.com/'
-    const httpService = new HttpService();
-    const response = await httpService.get(`${API_BASE_URL}freeGamesPromotions?locale=${locale.language}&country=${locale.countryCode}&allowCountries=${locale.countryCode}`);
-    return response.reduce((previousValue: Array<ResultEpic>, currentValue) => {
-        const resultEpic = ResultEpic.fromJSON(currentValue);
+    const API_BASE_URL: string = 'https://store-site-backend-static-ipv4.ak.epicgames.com/'
+    const httpService: HttpService = new HttpService();
+    const response: Array<IResult> = await httpService.get(`${API_BASE_URL}freeGamesPromotions?locale=${locale.language}&country=${locale.countryCode}&allowCountries=${locale.countryCode}`);
+    return response.reduce((previousValue: Array<ResultEpic>, currentValue: IResult) => {
+        const resultEpic: ResultEpic = ResultEpic.fromJSON(currentValue);
         if (resultEpic.data.price.totalPrice.discountPrice == 0 && resultEpic.data.price.totalPrice.discount > 0) {
             previousValue.push(resultEpic);
         }
--- src/repositories/httpService.ts
+++ src/repositories/httpService.ts
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2023 Allan Pereira <https://github.com/allanpereira99>
 
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import HttpSereviceBase from './httpService_base';
 import { IResult } from '../interfaces/result_interface';
 
 export class HttpService extends HttpSereviceBase {
     async get(url: string): Promise<Array<IResult>> {
-        const res = await axios(url);
+        const res:AxiosResponse = await axios(url);
         return res.data.data.Catalog.searchStore.elements;
     };
 };
