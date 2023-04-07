9af35d2aada11dde6452c2ee12e1c9b18bba8a8f
allanpereira99 <allan.pereira.dev@gmail.com>
2023-04-07 14:29:28 -0300

    map some missing properties

--- src/interfaces/result_interface.ts
+++ src/interfaces/result_interface.ts
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2023 Allan Pereira <https://github.com/allanpereira99>
 
+interface IPrice {
+    totalPrice: ITotalPrice
+}
+interface ITotalPrice {
+    discountPrice: number,
+    originalPrice: number,
+    voucherDiscount: number,
+    discount: number,
+    currencyCode: string,
+    currencyInfo: ICurrencyInfo,
+    fmtPrice: IFmtPrice
+}
+interface ICurrencyInfo {
+    decimals: number
+}
+interface IFmtPrice {
+    originalPrice: string,
+    discountPrice: string,
+    intermediatePrice: string
+}
+interface IOfferMappings {
+    pageSlug: string,
+    pageType: string
+}
 export interface IResult {
     title: string
     id: string
@@ -8,21 +32,6 @@ export interface IResult {
     description: string
     keyImages: Array<string>
     price: IPrice
-    offerMappings: Array<Object>
+    offerMappings: Array<IOfferMappings>
     productSlug: string
-}
-
-interface IPrice {
-    totalPrice: ITotalPrice
-}
-
-interface ITotalPrice {
-    discountPrice: number,
-    originalPrice: number,
-    voucherDiscount: number,
-    discount: number,
-    currencyCode: string,
-    currencyInfo: object,
-    fmtPrice: object
-    totalPrice: object
 }
